a573d05bba608d36c0646ff83a38f89b0542a513
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2018-03-16 19:20:17 +0000

    added initial code

---
+++ HelloCppCalc/CalcApi/Controllers/CalcController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalcApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalcController : Controller
+    {
+        [DllImport(@"HelloCppCalc.dll", 
+            EntryPoint = "math_add", 
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern int Add(int a, int b);
+
+        // GET: api/Calc
+        [HttpGet]
+        public int Get(int a, int b)
+        {
+            return Add(a, b);
+        }
+    }
+}
---
+++ HelloCppCalc/HelloCppCalc/calc.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef MATH_HPP
+#define MATH_HPP
+extern "C"
+{
+	__declspec(dllexport) int __stdcall math_add(int a, int b) {
+		return a + b;
+	}
+}
+#endif
